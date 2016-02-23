f303768a93d24223e31acb098705acf1e7a7a9ac
Liam Bullard <apps@aetherum.com>
2016-02-23 22:24:20 +1100

    Added Message Sending Functionality

--- public/js/app.js
+++ public/js/app.js
@@ -7,3 +7,18 @@ socket.on('connect', function() {
 socket.on('message', function(message) {
     console.log('New Message: ' + message.text);
 });
+
+// Message Submit
+var $form = jQuery('#message-form');
+
+$form.on('submit', function(event) {
+    event.preventDefault();
+    
+    var $message = $form.find('input[name=message]');
+    
+    socket.emit('message', {
+       text: $message.val()
+    });
+    
+    $message.val('');
+});
